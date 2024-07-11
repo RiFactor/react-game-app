14ff345642051cf57a5229d9b3f939addecf2a8f
rk <khanri1@outlook.com>
2024-07-11 16:31:33 +0100

    set up and connect to BED

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import axios from "axios";
 import "./App.css";
+import GameCard from "./components/GameCard";
+import { useEffect, useState } from "react";
+// require("dotenv").config;
+
+type Game = {
+  id: number;
+  slug: string;
+  name: string;
+};
 
 function App() {
-  return <div>hi</div>;
+  // const apiKey = process.env.API_KEY;
+  // console.log({ apiKey });
+  // console.log(process.env);
+
+  const [games, setGames] = useState<Game[]>([]);
+  const baseUrl = "https://api.rawg.io/api";
+
+  useEffect(() => {
+    axios
+      .get(
+        //extract the api key to .env
+        `${baseUrl}/games?key=eb2ec1af874049fdb938b0a822c82e58`
+      )
+      .then((res) => {
+        // console.log(res.data.results);
+        setGames(res.data.results);
+      });
+  }, []);
+
+  console.log({ games });
+  return (
+    <div>
+      {games.map((game) => (
+        <div>{game.name}</div>
+      ))}
+      <GameCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/GameCard.tsx
@@ -0,0 +1,5 @@
+const GameCard = () => {
+  return <div>game</div>;
+};
+
+export default GameCard;
