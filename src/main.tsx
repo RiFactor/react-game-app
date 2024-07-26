6b3372e98f7f9b50affe2f9f71d46d8fe133622f
rk <khanri1@outlook.com>
2024-07-26 11:43:45 +0100

    implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GameDetailPage from "./pages/GameDetailPage.tsx";
 import NoPageFound from "./pages/NoPageFound.tsx";
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Routes>
           {/* Layout */}
