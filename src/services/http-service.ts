b3024ea39cf3767b4d866a0dd113d3788fbf245e
rk <khanri1@outlook.com>
2024-07-26 10:38:01 +0100

    http-service to try

---
+++ src/services/http-service.ts
@@ -0,0 +1,34 @@
+import { keyString } from "../constants/api";
+import apiClient from "./api-client";
+
+interface IProps {
+  selectedGenre?: string | undefined;
+  searchGameName?: string | undefined;
+  selectedPlatform?: string | undefined;
+}
+
+class HttpService {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll({ searchGameName, selectedGenre, selectedPlatform }: IProps) {
+    const controller = new AbortController();
+
+    const request = apiClient.get(`${this.endpoint}${keyString}`, {
+      // .get<Game[]>?
+      signal: controller.signal,
+      params: {
+        genres: selectedGenre,
+        search: searchGameName,
+        parent_platforms: selectedPlatform, // id can be string or number?
+      },
+    });
+    return { request, cancel: () => controller.abort() };
+  }
+}
+
+const create = (endpoint: string) => new HttpService(endpoint);
+
+export default create;
